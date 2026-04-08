a038940f1a7e5af97a43eb4ea00dbdc734456c40
ConfidenceEfem <confidenceefem1@gmail.com>
2026-04-08 13:56:35 +0100

    fix

--- index.js
+++ index.js
@@ -98,7 +98,6 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH? process.env.PUPPETEER_EXECUTABLE_PATH : null,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
